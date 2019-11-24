4ab203399fd2bdba0b51002df9c9710f4d980561
Michael Searle <msearle5@btinternet.com>
2019-11-24 15:34:35 +0000

    More reasonable weights for the novel and encyclopedia

--- src/objects.c
+++ src/objects.c
@@ -1275,10 +1275,10 @@ SPELL("blank paper", "plain", P_NONE, 9, 0, 0, 0, 0, HI_PAPER),
 /* tribute book for 3.6 */
 OBJECT(OBJ("novel", "paperback"),
        BITS(0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, P_NONE, PAPER),
-       0, SPBOOK_CLASS, 0, 0, 0, 20, 0, 0, 0, 1, 20, CLR_BRIGHT_BLUE),
+       0, SPBOOK_CLASS, 0, 0, 5, 20, 0, 0, 0, 1, 20, CLR_BRIGHT_BLUE),
 OBJECT(OBJ("encyclopedia", "hardback"),
       BITS(0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, P_NONE, PAPER),
-      0, SPBOOK_CLASS, 9, 0, 0, 300, 0, 0, 0, 1, 20, CLR_RED),
+      0, SPBOOK_CLASS, 9, 0, 30, 300, 0, 0, 0, 1, 20, CLR_RED),
 /* a special, one of a kind, spellbook */
 OBJECT(OBJ("Book of the Dead", "papyrus"),
        BITS(0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, P_NONE, PAPER),
